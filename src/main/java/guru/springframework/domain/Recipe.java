86e754b3ab7d9e611450610574dd2dc56451ec83
Hervé EVODA <herveevoda@gmail.com>
2024-06-03 01:27:47 +0200

    ManyToMany JPA Relashionship.

--- src/main/java/guru/springframework/domain/Recipe.java
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -22,11 +22,16 @@ class Recipe {
 
     @OneToMany(cascade = CascadeType.ALL , mappedBy = "recipe")
     private Set<Ingredient> ingredients;
-
     @Lob
     private Byte[] image;
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
+
+    @ManyToMany
+    @JoinTable(name = "recipe_category",
+               joinColumns = @JoinColumn(name = "recipe_id"),
+               inverseJoinColumns = @JoinColumn(name ="category_id"))
+    private Set<Category> categories;
 
     public Long getId() {
         return id;
@@ -122,5 +127,13 @@ class Recipe {
 
     public void setIngredients(Set<Ingredient> ingredients) {
         this.ingredients = ingredients;
+    }
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
     }
 }
