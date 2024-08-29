44e77cd1eee1532d29054fd01abfb8ee87ed1a32
Hervé EVODA <herveevoda@gmail.com>
2024-08-29 18:49:47 +0200

    Display a recipe by ID

--- src/main/java/guru/springframework/service/RecipeService.java
+++ src/main/java/guru/springframework/service/RecipeService.java
@@ -1,9 +1,12 @@
 package guru.springframework.service;
 
 import guru.springframework.domain.Recipe;
+import org.springframework.data.repository.CrudRepository;
 
 import java.util.Set;
 
-public interface RecipeService {
+public interface RecipeService  {
     Set<Recipe> getRecipes();
+
+    Recipe findById(Long l);
 }
